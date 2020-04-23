e02689a3ba9dcb7834ed63d21ea41e65599c594f
mawaskow <61253606+mawaskow@users.noreply.github.com>
2020-04-23 15:16:45 -0700

    import rasterio library and changed file location

---
+++ Scripts/CHMCalculation.py
@@ -0,0 +1,22 @@
+'''
+Author: Ales Waskow
+Affiliation: VIP Lab
+Date: 23 April 2020
+Topic: Feasibility of using python for CHM calculation
+Description:
+This program is a means of determining the script necessary to calculate 
+CHM from DSM and DTM/DEM.
+'''
+
+# import libraries
+import rasterio
+
+# function definitions
+
+
+
+####################################
+def main():
+
+
+main()
