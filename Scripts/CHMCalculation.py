840bddaafd18051e0b86f7bfdcbc6bf1dd9e1f1f
mawaskow <61253606+mawaskow@users.noreply.github.com>
2020-04-23 17:18:44 -0700

    initialized possible libraries to use for tiff files in python

--- Scripts/CHMCalculation.py
+++ Scripts/CHMCalculation.py
@@ -9,14 +9,33 @@ CHM from DSM and DTM/DEM.
 '''
 
 # import libraries
-import rasterio
+import matplotlib.pyplot as plotter
+from osgeo import gdal
+import numpy
+import geopandas
+from skimage import io
+import skimage.external.tifffile
 
 # function definitions
 
+def use_matplot(filetiff):
+    image = plotter.imread(filetiff)
+    
+    plotter.imsave(filetiff, ar)
 
+def use_matplot_and_skimage(filetiff, outfiletiff):
+    image = io.imread(filetiff)
+    plotter.imshow(image, cmap = black)
+    plotter.axis("off")
+    plotter.savefig(outfiletiff, transparent = True, dpi = 300, bbox_inches = "tight", pad_inches= 0.0)
+
+def use_gdal(filetiff):
+    tiffdata = gdal.Open(filetiff, 'r')
+    n = tiffdata.
+    for i in range(1, )
 
 ####################################
 def main():
-
+    use_matplot("C:\Users\MAWaskow\Documents\Research\Spring20\Coding\SantaRita20170708DSM.tif")
 
 main()
