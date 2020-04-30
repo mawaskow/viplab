d60a08c55e652a35aef68f30fa1ba81812313f79
mawaskow <mawaskow@email.arizona.edu>
2020-04-30 21:08:25 +0000

    script to calculate ndvi from a folder of orthophotos (currently only calculates first orthophoto's ndvi)

---
+++ Scripts/NDVIfromfolder.R
@@ -0,0 +1,34 @@
+##### install necessary packages #####
+#install.packages("raster")
+#install.packages("rgdal")
+#install.packages("rgeos")
+#install.packages("RColorBrewer")
+
+# libraries
+library(raster)
+library(rgdal)
+library(rgeos)
+library(RColorBrewer)
+###
+
+generate_ndvi <- function(orthophotoinfile, ndvioutfile){
+  options(stringsAsFactors = FALSE)
+  orthoforndvi <- stack(orthophotoinfile)
+  orthoforndvi_brick <- brick(orthoforndvi)
+  r_brick <- orthoforndvi_brick[[1]]
+  nir_brick <- orthoforndvi_brick[[4]]
+  ndvicalc <- (nir_brick - r_brick) / (nir_brick + r_brick)
+  writeRaster(x = ndvicalc,
+              filename= ndvioutfile, format = "GTiff", datatype='INT2S', overwrite = TRUE)
+}
+
+### Here is where you select the folder from which you will select the files
+filenames <- c(list.files("Data", pattern = "*orthophoto.tif", full.names = 'True'))
+
+for (i in seq_along(filenames)){
+  len = nchar(filenames[i])
+  begin = substr(filenames[i], 1, len-14)
+  end = "NDVI.tif"
+  ndviname = paste(begin, end, sep = "")
+  generate_ndvi(filenames[i], ndviname)
+}
